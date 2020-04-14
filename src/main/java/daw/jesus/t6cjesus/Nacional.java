21ceb10478b150f09c8a12bfb4b4e4eefe16ebf8
jesusledesma15 <jesusproint998@gmail.com>
2020-04-14 19:13:45 +0200

    Añade toString()

--- src/main/java/daw/jesus/t6cjesus/Nacional.java
+++ src/main/java/daw/jesus/t6cjesus/Nacional.java
@@ -5,18 +5,21 @@
  */
 package daw.jesus.t6cjesus;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Jesus
  */
-public class Nacional {
-    
+public class Nacional extends Policia {
+
     private String arma;
     private String coche;
     private String pais;
     private int numEmergencia;
 
-    public Nacional(String arma, String coche, String pais, int numEmergencia) {
+    public Nacional(String arma, String coche, String pais, int numEmergencia, int nPlaca, LocalDate inicio, String nombre, String apellidos, String nif, int tlf) {
+        super(nPlaca, inicio, nombre, apellidos, nif, tlf);
         this.arma = arma;
         this.coche = coche;
         this.pais = pais;
@@ -54,6 +57,10 @@ public class Nacional {
     public void setNumEmergencia(int numEmergencia) {
         this.numEmergencia = numEmergencia;
     }
-    
-    
+
+    @Override
+    public String toString() {
+        return super.toString() + " arma=" + arma + ", coche=" + coche + ", pais=" + pais + ", numEmergencia=" + numEmergencia + '}';
+    }
+
 }
