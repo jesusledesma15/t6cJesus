25e6c9bc25546596bebe7708c4144accf1275452
jesusledesma15 <jesusproint998@gmail.com>
2020-04-14 19:06:16 +0200

    Añade clase Nacional

---
+++ src/main/java/daw/jesus/t6cjesus/Nacional.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.jesus.t6cjesus;
+
+/**
+ *
+ * @author Jesus
+ */
+public class Nacional {
+    
+    private String arma;
+    private String coche;
+}
