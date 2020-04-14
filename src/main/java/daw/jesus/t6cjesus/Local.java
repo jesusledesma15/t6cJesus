706e84af9977b747749ca2d23a493edf4d46f584
jesusledesma15 <jesusproint998@gmail.com>
2020-04-14 19:15:01 +0200

    Añade clase Local

---
+++ src/main/java/daw/jesus/t6cjesus/Local.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.jesus.t6cjesus;
+
+/**
+ *
+ * @author Jesus
+ */
+public class Local extends Policia {
+    
+    private String arma;
+    private String coche;
+    private String Localidad;
+    private int numEmergencia;
+    
+    
+}
