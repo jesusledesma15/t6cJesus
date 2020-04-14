5af38c05961732ae0bdd374d8635f3db7488d7c8
jesusledesma15 <jesusproint998@gmail.com>
2020-04-14 19:15:31 +0200

    Añade constructor parametrizado

--- src/main/java/daw/jesus/t6cjesus/Local.java
+++ src/main/java/daw/jesus/t6cjesus/Local.java
@@ -5,6 +5,8 @@
  */
 package daw.jesus.t6cjesus;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Jesus
@@ -15,6 +17,14 @@ public class Local extends Policia {
     private String coche;
     private String Localidad;
     private int numEmergencia;
+
+    public Local(String arma, String coche, String Localidad, int numEmergencia, int nPlaca, LocalDate inicio, String nombre, String apellidos, String nif, int tlf) {
+        super(nPlaca, inicio, nombre, apellidos, nif, tlf);
+        this.arma = arma;
+        this.coche = coche;
+        this.Localidad = Localidad;
+        this.numEmergencia = numEmergencia;
+    }
     
     
 }
